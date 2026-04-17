2ed7ca85e7ce8a72a409bb3028c3ed08a385f670
Nnomenja <mamenosoa42@gmail.com>
2026-04-17 16:04:39 +0300

    feat: add UnsupportedMediaType exception for handling 415 errors

---
+++ Exception/UnsupportedMediaType.hpp
@@ -0,0 +1,11 @@
+#ifndef UNSUPPORTEDMEDIATYPE_HPP
+#define UNSUPPORTEDMEDIATYPE_HPP
+
+#include "ServerException.hpp"
+
+class UnsupportedMediaType: public ServerException {
+    public:
+        UnsupportedMediaType():ServerException(415, "Unsupported Media Type"){};
+};
+
+#endif
